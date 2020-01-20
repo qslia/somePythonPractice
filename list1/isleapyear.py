8141c2641eed0f517d5f85d3d9757bfefd3ea8d2
Qslia <49663251+qslia@users.noreply.github.com>
2020-01-20 14:49:33 +0800

    Add files via upload

---
+++ list1/isleapyear.py
@@ -0,0 +1,13 @@
+year = int(input("请输入一个年份："))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("{0}是闰年".format(year))
+        else:
+            print("{0}不是闰年".format(year))
+    else:
+        print("{0}是闰年".format(year))
+else:
+    print("{0}不是闰年".format(year))
+
+
